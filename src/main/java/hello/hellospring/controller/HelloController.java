43b5ded24d3fdf0ea302bb81b4b960f08dbea417
Kimginam97 <65409092+Kimginam97@users.noreply.github.com>
2020-12-24 12:05:42 +0900

    test

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    //정적
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","hello!");
